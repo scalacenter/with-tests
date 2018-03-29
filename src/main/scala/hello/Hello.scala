3be26f4f21427c5bc0b83deb96d6e66973102eb2
jvican <jorgevc@fastmail.es>
2018-03-29 17:32:02 +0200

    Add use of environment variable
    ... to check that we pass in the correct environment variables from
    bloop.

--- src/main/scala/hello/Hello.scala
+++ src/main/scala/hello/Hello.scala
@@ -1,6 +1,9 @@
 package hello
 
 object Hello {
-  def greet(name: String): String =
+  def greet(name: String): String = {
+    // We do this to check that environment variables work
+    sys.env.get("BLOOP_OWNER").getOrElse(sys.error("Missing BLOOP_OWNER env variable!"))
     s"Hello, $name!"
+  }
 }
